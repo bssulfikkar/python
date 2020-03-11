0bf72aebb59d30379b246522669ba4de8b9a0686
bssulfikkar <56513796+bssulfikkar@users.noreply.github.com>
2020-03-11 16:52:47 +0100

    Update and rename demo.py to nthroot.py

--- nthroot.py
+++ nthroot.py
@@ -2,19 +2,19 @@
 #call the python script  using the below command inside the REPL
 #import os
 #import subprocess
-#DIR = os.path.join('C:\\', 'Users', 'K', 'Desktop', 'demo.py')
+#DIR = os.path.join('C:\\', 'Users', 'K', 'Desktop', 'nthroot.py')
 #subprocess.call(['python', DIR])
-#from demo import nth_root
+#from nthroot import nth_root
 #nth_root(16,2)
-#from demo import ordinal
+#from nthroot import ordinal
 #ordinal(2)
-#from demo import display_nth_root
+#from nthroot import display_nth_root
 #display_nth_root(16,2)
 
 #call the function inside the python script from windows using the below commands
-#python -c 'import demo; demo.nth_root(16, 2)'
-#python -c 'import demo; demo.ordinal(2)'
-#python -c 'import demo; demo.display_nth_root(16,2)'
+#python -c 'import nthroot; nthroot.nth_root(16, 2)'
+#python -c 'import nthroot; nthroot.ordinal(2)'
+#python -c 'import nthroot; nthroot.display_nth_root(16,2)'
 #####################################################
 
 def nth_root(radicand, n):
